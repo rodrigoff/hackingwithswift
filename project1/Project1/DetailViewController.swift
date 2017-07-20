f00ce1c49e0affd2643ec9e88220124d052df9ef
Rodrigo F. Fernandes <rodrigoff@live.com>
2017-07-20 14:24:12 -0300

    Adds project1

---
+++ project1/Project1/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Rodrigo F. Fernandes on 7/20/17.
+//  Copyright © 2017 Rodrigo F. Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+
+        
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
---
+++ project1/README.md
@@ -0,0 +1,5 @@
+# Project 1: Storm viewer
+
+![](content/project1_1.png)
+
+![](content/project1_2.png)
