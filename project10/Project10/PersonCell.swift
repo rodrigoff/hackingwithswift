22f8105d64cd60d948c232ae956637a62cca2550
Rodrigo F. Fernandes <rodrigoff@live.com>
2017-07-21 16:29:13 -0300

    Adds project10

---
+++ project10/Project10/PersonCell.swift
@@ -0,0 +1,18 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Rodrigo F. Fernandes on 7/21/17.
+//  Copyright © 2017 Rodrigo F. Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
---
+++ project10/README.md
@@ -0,0 +1,11 @@
+# Project 10: Names to Faces
+
+![](content/project10_1.png)
+
+![](content/project10_2.png)
+
+![](content/project10_3.png)
+
+![](content/project10_4.png)
+
+![](content/project10_5.png)
