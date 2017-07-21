6e76f78db187e19ef3809fe29a07ccb03e9bc1c2
Rodrigo F. Fernandes <rodrigoff@live.com>
2017-07-21 11:32:38 -0300

    Adds project7

---
+++ project7/Project7/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by Rodrigo F. Fernandes on 7/21/17.
+//  Copyright © 2017 Rodrigo F. Fernandes. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ project7/README.md
@@ -0,0 +1,7 @@
+# Project 7: Whitehouse Petitions
+
+![](content/project7_1.png)
+
+![](content/project7_2.png)
+
+![](content/project7_3.png)
